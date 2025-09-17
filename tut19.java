2e9256a0113dcefbb5ee16009ccf4bc938ac4e90
prince singh <princesingh36006457@gmail.com>
2025-09-17 23:26:11 +0530

    firstcommit

---
+++ tut19.java
@@ -0,0 +1,9 @@
+//Array 
+public class tut19 {
+    public static void main (String a[]){
+        int num[]={1,2,3,4};
+        for(int i=0;i<4;i++){
+        System.out.println(num[i]);
+        }
+    }
+}
