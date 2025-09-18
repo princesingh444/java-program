d55f27c3593a49a8da135ac553da84415e4ca985
prince singh <princesingh36006457@gmail.com>
2025-09-18 14:52:20 +0530

    firstcommit

---
+++ tut24.java
@@ -0,0 +1,13 @@
+public class tut24 {
+    public static void main(String a[]){
+       int arr[]=new int [4];
+       arr[0]=1;
+       arr[1]=2;
+       arr[2]=3;
+       arr[3]=4;
+       for( int n:arr){ //this is called enhanced for loop in java ..
+        System.out.println(n);
+       }
+
+    }
+}
