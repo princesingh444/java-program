4bf8f27c737cca0f9f3922720da427fbb520f635
prince singh <princesingh36006457@gmail.com>
2025-09-18 22:48:35 +0530

    firstcommit

---
+++ tut25.java
@@ -0,0 +1,14 @@
+// using String 
+public class tut25 {
+    public static void main(String a[]){
+        String name= "prince";   /*string is a data type .
+        which hold the combination of character and it is located by " " ...*/
+        System.out.println(name);
+        System.out.println("hello "+ name); // String only use + operatot no other operator use 
+        System.out.println(name.hashCode());  // hashCode give the data
+        System.out.println(name.charAt(4)); /*charAT use for index num from String .
+        it is similar than compare to array index num consept.. */
+        System.out.println(name.concat(" singh"));
+        // concat is use for add two word ..
+    }
+}
