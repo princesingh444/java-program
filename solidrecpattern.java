2b3041f24a4ba7116f15f76cec348a7f4c9a0fed
prince singh <princesingh36006457@gmail.com>
2025-09-21 09:43:56 +0530

    pattern

---
+++ solidrecpattern.java
@@ -0,0 +1,11 @@
+// Solid rectangle pattern 
+public class solidrecpattern {
+    public static void main (String a[]){
+        for (int i=0;i<4;i++){
+            for(int j=1;j<5;j++){
+                System.out.print("*");
+            }
+            System.out.println("*");
+        }
+    }
+}
