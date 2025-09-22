8e1bf099ea6086ce672b968e04bf4bfb932cde91
prince singh <princesingh36006457@gmail.com>
2025-09-22 21:37:22 +0530

    firstcommit

--- Hello.java
+++
@@ -1,6 +0,0 @@
-class Hello {  
-    public static void main(String a[]) {
-        System.out.println("hello world... ");
-        System.out.println("my name is prince ");
-    }
-}
--- tut28.java
+++ tut28.java
@@ -1,7 +1,7 @@
 class mobile
 {
     int price;
-    String name;
+    static String name;       //static are use to print same value 
     String brand;
 
     public void show(){ // it's a instance method .
@@ -14,13 +14,13 @@ public class tut28 {
     public static void main (String a[]){
         mobile obj1=new mobile ();
         obj1.price=100000;
-        obj1.name="smartphone";
+        obj1.name="smartphone";   
         obj1.brand="iphone";
 
         mobile obj2=new mobile ();
         obj2.price=150000;
-        obj2.name="smartphone";
-        obj2.brand="sumsung";
+        obj2.name="phone";      // because phone call by later.
+        obj2.brand="sumsung";   // so the name is phone not a smartphone .
 
         obj1.show();
         obj2.show();
