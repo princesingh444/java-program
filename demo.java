573723ee8113fd6ab6c8105ec54d885a954f0c5a
prince singh <princesingh36006457@gmail.com>
2025-09-24 11:02:17 +0530

    firstcommit

---
+++ demo.java
@@ -0,0 +1,11 @@
+
+public class demo{
+public static void main (String a[]){
+       advcalc obj= new advcalc ();
+       int r1=obj.add(3, 9);
+       int r2=obj.sub(21,3);
+       int r3=obj.multi(5,4);
+       int  r4=obj.div(23,4);
+       System.out.println(r1+" "+ r2+ " "+r3+" "+r4+" ");
+}
+}
