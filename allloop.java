5a768eba3e9c0147b2ee32362161ae81a99ae82a
prince singh <princesingh36006457@gmail.com>
2025-09-26 15:13:09 +0530

    loop

---
+++ allloop.java
@@ -0,0 +1,49 @@
+public class allloop {
+    public static void main (String a[]){
+        // #####  for loop   ######### 
+        // for (initialisation;condition;updation)
+
+        // #### count 1 to 20  #########
+        // for(int n=0;n<=20;n++){
+        //     System.out.println(n);
+        // }
+
+        //######### while loop ##### 
+
+        // int i=0;
+        // while (i<=20){
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+
+        // ##### do while loop ######
+
+        // int i=1;
+        // do{
+        //     System.out.println(i);
+        //     i++;
+        // }while(i<=20);
+
+
+
+
+        //##### sum of first n natural number ########
+// int sum =0;
+//         for(int i=1;i<=4;i++){
+//             sum=sum+i;
+
+//         }System.out.println(sum);
+
+
+//###### table of number ######
+
+int n=1;
+for(int i=0;i<=20;i+=2){
+    System.out.println(i);
+}
+
+
+    }
+
+}
